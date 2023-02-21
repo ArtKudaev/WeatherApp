50dbe61eaa37a50d035264f352d4d01181d0bd08
ArtKudaev <oldoldskoolgamer@gmail.com>
2023-02-21 21:16:20 +0300

    Weather app - trying to create addList

--- wApp.js
+++ wApp.js
@@ -8,14 +8,14 @@ function fetchRequest () {
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
     
     if (cityName === '') return;
-
+    
     fetch(url)
         .then(response => {
             if (response.ok) {
                 return response.json()
             }
             else {
-                console.log("ERROR")
+                console.log("ERROR");
                 throw Error
             }
         })
@@ -23,20 +23,59 @@ function fetchRequest () {
             console.log(data)
             document.querySelector('.city__name').innerHTML = data.name;
             document.querySelector('.city__celcium').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
-            let icon = data.weather[0].icon
+            let icon = data.weather[0].icon;
             document.querySelector('.city__weather-pic').src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
-        }).catch(error => {
-            console.log(error)
+        }).catch((error) => {
+            alert('УАХЬ!!!');
         })
 }
 
 input.addEventListener('keyup', e => {
     if (e.keyCode === 13) {
         fetchRequest ()
-        }
+    }
 });
 
 btnRequest.addEventListener('click', () => {
         fetchRequest ()
     }
 );
+
+const addButton = document.querySelector('.city__add-button');
+
+addButton.addEventListener ('click', () => {
+    const addedList = document.createElement('dl');
+    addedList.className = 'added-locs__list';
+    const addedLoc = createLocsLicst();
+    addedList.append(addedLoc);
+});
+
+function createLocsLicst () {    
+    const addedListItem = document.createElement('dt');
+    addedListItem.className = 'added-locs__list-item';
+    const addedCityName = document.querySelector('.city__name').innerHTML;
+    addedListItem.textContent = addedCityName;
+
+    const locDelBtn = document.createElement('button');
+    locDelBtn.className = 'added-locs__delBtn';
+    locDelBtn.type = 'image';
+    locDelBtn.src = 'delBtn.png';
+
+    locDelBtn.addEventListener('click', (e) => {
+        deleteAddedLoc(e.target);
+    });
+
+    const addedLoc = document.createElement('div');
+    addedLoc.className = 'added-locs__item-block';
+    
+    addedLoc.append(addedListItem, locDelBtn);
+    
+    return addedLoc;
+};
+
+function deleteAddedLoc (target) {
+    const addedList = target.closest('.added-locs__list');
+    const addedLoc = target.closest('.added-locs__item-block');  
+    
+    addedList.removeChild(addedLoc);
+};
